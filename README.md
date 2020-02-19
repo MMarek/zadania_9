42666d0d09bd92bd4dc50f650304e070691a2a26
MMarek <mildnermarek@gmail.com>
2020-02-19 16:47:37 +0100

    mathematical operations corrections

--- README.md
+++ README.md
@@ -6,3 +6,16 @@ Kod który renderuje na stronie treść powitania oraz imię, przypisane do do z
 #
 ### -zad.2
 Po załadowaniu strony, użytkownik podaje dwie liczby, które następnie są wyświetlone, oraz dodane do siebie.
+
+#
+### -zad.3
+Pobiera rok urodzenia od użytkownika i wypisuje na stronie jego wiek.
+
+#
+### -zad.4
+Wypisuje na stronie wywołaną przez kod postać, której dane zostały umieszczone w innym wyeksportowanym pliku.
+
+#
+### -zad.5
+Prosty kalkulator - pobiera od użytkownika dwie liczby, oraz operacje która ma zostać wykorzystana na tych liczbach. 
+Podaje wynik na stronie.
--- src/components/task05.js
+++ src/components/task05.js
@@ -8,6 +8,17 @@ class Task05 extends Component {
         let num2 = parseFloat(prompt('Wprowadź drugą liczbę'));
         let operation = prompt('Wprowadź operację (+ , - , * , /)');
 
+        let jsx;
+        if (operation === '+') {
+            jsx = <h1>{num1} + {num2} = {num1 + num2}</h1>
+        } else if (operation === '-') {
+            jsx = <h2>{num1} - {num2} = {num1 - num2}</h2>
+        } else if (operation === '*') {
+            jsx = <h3>{num1} * {num2} = {num1 * num2}</h3>
+        } else if (operation === '/') {
+            jsx = <h4>{num1} / {num2} = {num1 / num2}</h4>
+        }
+        return jsx;
     }
 }
 
