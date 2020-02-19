5efdfecbfa8e74dd39515509a65d6001416e9c43
MMarek <mildnermarek@gmail.com>
2020-02-19 16:31:50 +0100

    mathematical operations

--- src/components/home.js
+++ src/components/home.js
@@ -3,6 +3,7 @@ import Task01 from "./task01";
 import Task02 from "./task02";
 import Task03 from "./task03";
 import Task04 from "./task04";
+import Task05 from "./task05";
 
 
 
@@ -15,6 +16,7 @@ class Home extends Component {
             <Task02/>
             <Task03/>
             <Task04/>
+            <Task05/>
             </>
         )
     }
---
+++ src/components/task05.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+
+class Task05 extends Component {
+
+    render() {
+        let num1 = parseFloat(prompt('Wprowadź pierwszą liczbę'));
+        let num2 = parseFloat(prompt('Wprowadź drugą liczbę'));
+        let operation = prompt('Wprowadź operację (+ , - , * , /)');
+
+    }
+}
+
+export default Task05;
